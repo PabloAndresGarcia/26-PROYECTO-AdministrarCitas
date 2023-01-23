e72673b4901fc4fa29050ad1c2832c4784b405bb
Pablo Andres Garcia <garciasjs@gmail.com>
2023-01-23 10:12:27 -0300

    1

--- js/app.js
+++ js/app.js
@@ -16,6 +16,9 @@ class citas{
     agregarCita(cita) {
         this.citas = [...this.citas, cita];
     }
+    elimnarCita(id){
+      this.citas = this.citas.filter( cita => cita.id !== id )
+    }
 }
 
 class UI{
@@ -77,6 +80,16 @@ class UI{
 
             const sintomasParrafo = document.createElement('p');
             sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Sintomas: </span> ${sintomas}`;
+
+            //voton para eliminar citas
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>';
+            btnEliminar.onclick = () => {
+                eliminarCita(id)
+            }
+            
                      
             //Agregar parrafo al divCita
 
@@ -86,6 +99,7 @@ class UI{
             divCita.appendChild(fechaParrafo);
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar);
 
             contenedorCitas.appendChild(divCita)
         })
@@ -166,4 +180,13 @@ function reiniciarObjeto(){
     citaObj.fecha= '';
     citaObj.hora= '';
     citaObj.sintomas= '';
+}
+
+function elimnarCita(id){
+  //Eliminar mensaje
+    administrarCitas.elimnarCita(id)
+  //Mueste un mensaje
+    ui.imprimirAlerta('La cita se elimino correctamente');
+  //refrescar citas
+    ui.imprimirCitas(administrarCitas);
 }
